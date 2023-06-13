75da2b729a31baeb212f22c440a9ca3107080dd0
ryinner <vinograd18062002@gmail.com>
2023-06-13 10:43:16 +0500

    plugin

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import createWebSocket from '@ryinner/web-socket-manager';
+import { inject, type Plugin } from 'vue';
+
+let ws!: ReturnType<typeof createWebSocket>;
+
+const plugin: Plugin<Parameters<typeof createWebSocket>> = {
+    install: (app, wsSettings) => {
+        ws = createWebSocket(wsSettings);
+
+        app.config.globalProperties.$webSocket = ws;
+        app.provide('$webSocket', ws);
+    }
+};
+
+declare module 'vue' {
+    interface ComponentCustomProperties {
+        $webSocket: typeof ws;
+    }
+}
+
+export { plugin as VueWebSocket };
+export function useWebSocketManager (): typeof ws | undefined {
+    return inject('$webSocket');
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,29 @@
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+const appDir = path.resolve(__dirname);
+const indexFile = path.join(appDir, 'src', 'index.ts');
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        dts()
+    ],
+    build: {
+        lib: {
+            entry: indexFile,
+            name: 'VueWebSocket',
+            fileName: 'index'
+        },
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue'
+                }
+            }
+        }
+    }
 });
